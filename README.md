60eacfb0deb3987a98ee27f236ffe8560210b802
billpmurphy <billpmurphy92@gmail.com>
2014-04-01 23:27:30 -0400

    not done yet, but more added

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Python wrapper for the [new 4chan API](https://github.com/4chan/4chan-API).
 Dependencies
 ------------
 
-- kennethreitz's [requests](https://github.com/kennethreitz/requests) library.
+- [requests](https://github.com/kennethreitz/requests) library by kennethreitz
 
 
 Installation
@@ -17,9 +17,46 @@ Nothing to install. Just `git clone` the source and then `import pychan` to use
 the API.
 
 
-API Tutorial
-------------
+Getting Started
+---------------
 
-Coming soon!
+```python
+from pychan import *
+g_board = Board("g")
+g_board.update_thread_index()
+g_board.get_threads()            # this might take a minute
+```
 
+If we are interested in a particular thread, we can model it with a Thread
+object:
 
+```python
+install_gentoo_thread = Thread("g", "39894014")     # /g/ wiki thread
+install_gentoo_thread.update()
+print "Sticky thread?  " + str(install_gentoo_thread.is_sticky())
+print "Closed thread?  " + str(install_gentoo_thread.is_closed())
+print "# of replies:   " + str(install_gentoo_thread.get_num_replies())
+```
+
+Similarly, we can track a particular page (e.g., the first page) on a board:
+
+```python
+a_front_page = Page("a", 0)
+a_front_page.update()
+```
+
+We can also retrieve basic metadata about all boards:
+
+```python
+board_list = BoardList()
+board_list.get_metadata()
+```
+
+We can iterate over a BoardList to play around with this metadata, like so:
+
+```python
+# print the names and titles of the worksafe boards
+for board in board_list:
+    if board.is_worksafe():
+        print "/%s/ - %s" % (board.get_name(), board.get_title())
+```
--- pychan.py
+++ pychan.py
@@ -10,7 +10,7 @@ CATALOG_INDEX = "a.4cdn.org/%s/threads.json"
 CATALOG_FULL = "a.4cdn.org/%s/catalog.json"
 BOARDS = "a.4cdn.org/boards.json"
 
-######################## Board/Thread/Post Info ########################
+######################## Board/Page/Thread/Post Info ########################
 
 class Board():
     def __init__(self, board_name, session=None, https=False):
@@ -22,18 +22,22 @@ class Board():
         else:
             self._session = session
 
+        self.pages = []
         self.thread_index = ThreadIndex(self.board_name, session=self._session, https=self._https)
         self.catalog = Catalog(self.board_name, session=self._session, https=self._https)
+    def __iter__(self):
+        return iter(self.thread_index.pages())
     def update_catalog(self, api = CATALOG_FULL):
         """
         Send a request and update the list of items in the Catalog object.
         """
         self.catalog.update()
-    def update_thread_index(self, api=CATALOG_INDEX):
+    def update(self, api=CATALOG_INDEX):
         """
         Send a request and update the list of items in the ThreadIndex object.
         """
-        self.thread_index.update()
+        for page in self.pages:
+            page.update()
     def get_threads(self, page=None):
         """
         Retrieve all data related to the threads currently in the thread index.
@@ -84,6 +88,8 @@ class ThreadIndex():
             self._url = "https://" + self._api
         else:
             self._url = "http://" + self._api
+    def __iter__(self):
+        return iter(self.pages)
     def update(self):
         """
         Send a request and update the index of current threads.
@@ -93,32 +99,81 @@ class ThreadIndex():
             if len(self.pages) == 0:
                 self.pages = [list() for page in res.json()]
             for page in res.json():
-                self.pages[page['page']] = \
-                    [Thread.from_index(self.board_name, t) for t in page['threads']]
+                self.pages[page["page"]] = \
+                    [Thread.from_index(self.board_name, t) for t in page["threads"]]
         else:
             res.raise_for_status()
 
 
 class Page():
-    def __init__(self, board, page_number):
-        pass
+    def __init__(self, board_name, page_number, session=None, api=PAGE, https=False):
+        self.board_name = board_name
+        self.page_number = page_number
+        self.threads = []
+        self._https = https
+        self._api = api % (self.board_name, self.page_number)
+
+        if session is None:
+            self._session = requests.session()
+            self._session.headers["User-Agent"] = "pychan"
+        else:
+            self._session = session
+
+        if self._https:
+            self._url = "https://" + self._api
+        else:
+            self._url = "http://" + self._api
+    def __iter__(self):
+        return iter(self.threads)
 
 
 class Thread():
-    def __init__(self, board, thread_id, last_modified):
-        pass
-    @classmethod
-    def fromJson(cls, json_data):
+    def __init__(self, board_name, thread_id, session=None, api=THREAD, https=False):
+        self.board_name = board_name
+        self.thread_id = thread_id
+        self._api = api % (self.board_name, self.thread_id)
+        self._https = https
+
+        if session is None:
+            self._session = requests.session()
+            self._session.headers["User-Agent"] = "pychan"
+        else:
+            self._session = session
+
+        if self._https:
+            self._url = "https://" + self._api
+        else:
+            self._url = "http://" + self._api
+
+        self.posts = []
+        self.closed = None
+        self.sticky = None
+        self.bumplimit = None
+        self.imagelimit = None
+        self.num_replies = None
+    def __iter__(self):
+        return iter(self.posts)
+    def update_from_json(self, json_data):
         """
-        Create a new Thread object from a request.
+        Updates the current Thread object from a JSON response to a thread
+        API query.
         """
-        pass
+        OP = json_data["posts"][0]
+        self.sticky = OP.get("sticky", None) == 1
+        self.closed = OP.get("closed", None) == 1
+        self.bumplimit = OP.get("bumplimit", None)
+        self.imagelimit = OP.get("imagelimit", None)
+        self.num_replies = OP.get("replies", 0)
+
+        self.posts = [[] for i in range(len(json_data["posts"]))]
+        for i in range(len(self.posts)):
+            self.posts[i] = Post(self, json_data["posts"][i])
     @classmethod
     def from_index(cls, board_name, thread_json):
         """
         Create a new Thread object from an entry in a ThreadIndex object.
         """
-        return cls(board_name, thread_json['no'], thread_json['last_modified'])
+        pass
     @classmethod
     def from_catalog(cls, board_name, catalog_json):
         """
@@ -130,7 +185,46 @@ class Thread():
         Sends a request and updates the Thread object if changes have been made
         to the thread.
         """
-        pass
+        res = self._session.get(self._url)
+        if res.status_code == 200:
+            if len(res.json()["posts"]) > len(self.posts):
+                self.update_from_json(res.json())
+        else:
+            res.raise_from_status()
+    def is_sticky(self):
+        """
+        Return True if the thread is sticky, and False otherwise.
+        Returns None if unknown.
+        """
+        return self.sticky
+    def is_closed(self):
+        """
+        Return True if the thread is closed, and False otherwise.
+        Returns None if unknown.
+        """
+        return self.closed
+    def get_posts(self):
+        """
+        Returns a list of all posts in the thread, or None if they are not
+        available.
+        """
+        return self.posts
+    def get_num_replies(self):
+        """
+        Returns the number of replies to the OP, or None if this is not
+        available.
+        """
+        return self.num_replies
+    def get_bump_limit(self):
+        """
+        Returns the current bump limit, or None if this is not available.
+        """
+        return self.bumplimit
+    def get_image_limit(self):
+        """
+        Returns the current image limit, or None if this is not available.
+        """
+        return self.imagelimit
 
 
 class Post():
@@ -147,9 +241,64 @@ class Post():
         self.comment = post_json.get("com", None)
         self.time = datetime.fromtimestamp(post_json.get("time", 0))
         self.timestamp = post_json.get("time", None)
-        self.is_closed = post_json.get("closed", None) == "1"
-        self.is_sticky = post_json.get("sticky", None) == "1"
 
+        if post_json.has_key("filename"):
+            self.file = Image(self.thread.board_name, post_json)
+        else:
+            self.file = None
+    def get_name(self):
+        """
+        Return the poster's name, if a name was entered.
+        """
+        return self.poster_name
+    def get_email(self):
+        """
+        Return the poster's email, if it exists.
+        """
+        return self.email
+    def get_number(self):
+        """
+        Return the post id number.
+        """
+        return self.post_number
+    def get_tripcode(self):
+        """
+        Return the poster's tripcode, if it exists.
+        """
+        return self.tripcode
+    def get_subject(self):
+        """
+        Return the subject field of the post.
+        """
+        return self.subject
+    def get_comment(self):
+        """
+        Return the comment (i.e., the body) of the post.
+        """
+        return self.comment
+    def get_time(self):
+        """
+        Return a datetime object corresponding to when the post was published.
+        """
+        return self.time
+    def has_image(self):
+        """
+        Return True if the post contains an image that has not been deleted,
+        otherwise return False.
+        """
+        if self.file is not None:
+            return not self.file.is_deleted()
+        return False
+    def get_image(self):
+        """
+        Return the post's image, if it has one. Otherwise, return None.
+        """
+        return self.file
+
+
+class Image():
+    def __init__(self, board_name, post_json, file_api=FILE, thumb_api=THUMBNAIL):
+        self.board_name = board_name
         self.filename = post_json.get("filename", None)
         self.file_id = post_json.get("tim", None)
         self.file_md5_hash = post_json.get("md5", None)
@@ -159,30 +308,62 @@ class Post():
         self.file_height = post_json.get("h", None)
         self.thumbnail_width = post_json.get("tn_w", None)
         self.thumbnail_height = post_json.get("tn_h", None)
-        self.file_deleted = post_json.get("file_deleted", None) == "1"
+        self.file_deleted = post_json.get("file_deleted", None) == 1
 
-        self.file_url = FILE % (self.thread.board.name, self.file_id + self.file_extension)
+        self.file_url = FILE % (self.board_name, str(self.file_id) + self.file_extension)
+        self.thumb_url = THUMBNAIL % (self.board_name, str(self.file_id))
     def get_file(self):
-        print self.file_url
+        if self.file_deleted:
+            raise(IOError("File was deleted."))
+        else:
+           pass
     def get_thumbnail(self):
-        pass
-
-
-
+        if self.file_deleted:
+            raise(IOError("File was deleted."))
+        else:
+            pass
+    def get_filename(self):
+        return self.filename
+    def get_file_id(self):
+        return self.file_id
 
 
 ######################## Board Metadata Info ########################
 
 class BoardMetadata():
     def __init__(self, json):
-        self.title = json["title"]
-        self.board = json["board"]
+        self.board_title = json["title"]
+        self.board_name = json["board"]
         self.threads_per_page = int(json["per_page"])
         self.num_pages = int(json["pages"])
-        self.worksafe = True if json["ws_board"] == "1" else False
+        self.worksafe = True if json["ws_board"] == 1 else False
     def __str__(self):
-        return self.board
-
+        return self.board_name
+    def get_name(self):
+        """
+        Returns the name of the board, e.g. /b/ or /g/.
+        """
+        return self.board_name
+    def get_title(self):
+        """
+        Returns the title of the board, e.g. Random or Technology.
+        """
+        return self.board_title
+    def get_num_threads_per_page(self):
+        """
+        Returns the number of threads per page (as an int).
+        """
+        return self.threads_per_page
+    def get_num_pages(self):
+        """
+        Returns the number of pages in the board (as an int).
+        """
+        return self.num_pages
+    def is_worksafe(self):
+        """
+        Returns True if the board is Safe For Work, and False otherwise.
+        """
+        return self.worksafe
 
 class BoardList():
     def __init__(self, api=BOARDS, session=None, https=False):
@@ -200,10 +381,15 @@ class BoardList():
             self._url = "https://" + self._api
         else:
             self._url = "http://" + self._api
-    def get_boards(self):
+    def __iter__(self):
+        return iter(self.board_list)
+    def get_metadata(self):
+        """
+        Retrieves the metadata for all boards.
+        """
         res = self._session.get(self._url)
         if res.status_code == 200:
-            for board_json in res.json()['boards']:
+            for board_json in res.json()["boards"]:
                 self.board_list.append(BoardMetadata(board_json))
         else:
             res.raise_for_status()
